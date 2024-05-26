73f2995a5fba8bf282f743f094e8aae62b27d6ce
Gabriel <gabrielsilveira@id.uff.br>
2024-05-26 01:48:41 -0300

    fix(network): fix colyseus sync

--- apps/server/src/Room.ts
+++ apps/server/src/Room.ts
@@ -1,15 +1,5 @@
-import { MapSchema, Schema, type } from '@colyseus/schema';
+import { Player, RoomState } from '@ppog/shared';
 import { Client, Room } from 'colyseus';
-
-export class Player extends Schema {
-  @type('string') username: string;
-  @type('number') x: number;
-  @type('number') y: number;
-}
-
-export class RoomState extends Schema {
-  @type({ map: Player }) players = new MapSchema<Player>();
-}
 
 export class MainRoom extends Room<RoomState> {
   maxClients: number = 50;
@@ -23,8 +13,8 @@ export class MainRoom extends Room<RoomState> {
       const player = this.state.players.get(client.sessionId);
 
       if (player) {
-        player.x = message.x;
-        player.y = message.y;
+        player.position.x = message.x;
+        player.position.y = message.y;
       }
     });
   }
@@ -33,8 +23,8 @@ export class MainRoom extends Room<RoomState> {
     console.log(client.sessionId, 'joined!');
     const player = new Player();
     player.username = 'Guest';
-    player.x = Math.floor(Math.random() * 100);
-    player.y = Math.floor(Math.random() * 100);
+    player.position.x = Math.floor(Math.random() * 100);
+    player.position.y = Math.floor(Math.random() * 100);
     this.state.players.set(client.sessionId, player);
   }
 
